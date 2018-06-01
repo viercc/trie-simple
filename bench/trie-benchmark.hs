ee1c9708d77337a24d8aad72dc209217837294d9
Koji Miyazato <viercc@gmail.com>
2018-06-01 14:48:28 +0900

    Improve benchmark
    - Add insert, delete, alter benchmark
    - Change unnecessary 'nf' to 'whnf'

--- bench/trie-benchmark.hs
+++ bench/trie-benchmark.hs
@@ -46,37 +46,38 @@ randomStrs =
 main :: IO ()
 main = defaultMain [ benchTSet, benchSet, benchTMap, benchMap ]
 
-enumerateViaFoldr :: TSet.TSet c -> [[c]]
-enumerateViaFoldr = TSet.foldr (:) []
-
 benchTSet :: Benchmark
 benchTSet = bgroup "TSet" 
   [ bgroup "construction"
-      [ env dictAmEnShuffled $ \dict -> bench "fromList" $ nf TSet.fromList dict
+      [ env dictAmEnShuffled $ \dict ->
+          bench "fromList" $ whnf TSet.fromList dict
       , env (sort <$> dictAmEn) $ \sortedDict ->
-          bench "fromAscList" $ nf TSet.fromAscList sortedDict ]
+          bench "fromAscList" $ whnf TSet.fromAscList sortedDict ]
   , env (TSet.fromList <$> dictAmEn) $ \dict ->
       bgroup "query"
         [ bench "isEmpty" (nf TSet.null dict)
         , bench "stringCount" (nf TSet.count dict)
         , bench "enumerate10" (nf (take 10 . TSet.enumerate) dict)
-        , bench "enumerate10_foldr" (nf (take 10 . enumerateViaFoldr) dict)
-        , bench "enumerate100" (nf (take 100 . TSet.enumerate) dict)
-        , bench "enumerate100_foldr" (nf (take 100 . enumerateViaFoldr) dict)
         , bench "enumerateAll" (nf TSet.enumerate dict)
-        , bench "enumerateAll_foldr" (nf enumerateViaFoldr dict)
         , env randomStrs $ \qs ->
             bench "match" (nf (\dict' -> map (`TSet.member` dict') qs) dict) ]
+  , env (TSet.fromList <$> dictAmEn) $ \dict ->
+      bgroup "single-item"
+        [ bench "insert1" (whnf (TSet.insert "wwwwwwwwwwwwwwww") dict)
+        , bench "insert2" (whnf (TSet.insert "cheese") dict)
+        , bench "delete1" (whnf (TSet.delete "wwwwwwwwwwwwwwww") dict)
+        , bench "delete2" (whnf (TSet.delete "cheese") dict)
+        ]
   , env (TSet.fromList <$> dictAmEn) $ \dictA ->
     env (TSet.fromList <$> dictBrEn) $ \dictB ->
     env (TSet.fromList <$> randomStrs) $ \dictSmall ->
       bgroup "combine"
-        [ bench "union" (nf (uncurry TSet.union) (dictA, dictB))
-        , bench "intersection" (nf (uncurry TSet.intersection) (dictA, dictB))
-        , bench "difference" (nf (uncurry TSet.difference) (dictA, dictB))
-        , bench "append" (nf (uncurry TSet.append) (dictSmall, dictSmall))
-        , bench "prefixes" (nf TSet.prefixes dictA)
-        , bench "suffixes" (nf TSet.suffixes dictB) ]
+        [ bench "union" (whnf (uncurry TSet.union) (dictA, dictB))
+        , bench "intersection" (whnf (uncurry TSet.intersection) (dictA, dictB))
+        , bench "difference" (whnf (uncurry TSet.difference) (dictA, dictB))
+        , bench "append" (whnf (uncurry TSet.append) (dictSmall, dictSmall))
+        , bench "prefixes" (whnf TSet.prefixes dictA)
+        , bench "suffixes" (whnf TSet.suffixes dictB) ]
   ]
 
 benchSet :: Benchmark
@@ -86,26 +87,35 @@ benchSet = bgroup "Set"
       -- and switch the algorithm based on it.
       -- Using shuffled dictionary avoids this optimization fires
       -- in this benchmark.
-      [ env dictAmEnShuffled $ \dict -> bench "fromList" $ nf Set.fromList dict
+      [ env dictAmEnShuffled $ \dict ->
+          bench "fromList" $ whnf Set.fromList dict
       , env (sort <$> dictAmEn) $ \sortedDict ->
-          bench "fromAscList" $ nf Set.fromAscList sortedDict ]
+          bench "fromAscList" $ whnf Set.fromAscList sortedDict ]
   , env (Set.fromList <$> dictAmEn) $ \dictSet ->
       bgroup "query"
         [ bench "isEmpty" (nf Set.null dictSet)
         , bench "stringCount" (nf Set.size dictSet)
         , bench "enumerate10" (nf (take 10 . Set.toList) dictSet)
+        , bench "enumerateAll" (nf Set.toList dictSet)
         , env randomStrs $ \qs ->
             bench "match" (nf (\dictSet' -> map (`Set.member` dictSet') qs) dictSet) ]
+  , env (Set.fromList <$> dictAmEn) $ \dict ->
+      bgroup "single-item"
+        [ bench "insert1" (whnf (Set.insert "wwwwwwwwwwwwwwww") dict)
+        , bench "insert2" (whnf (Set.insert "cheese") dict)
+        , bench "delete1" (whnf (Set.delete "wwwwwwwwwwwwwwww") dict)
+        , bench "delete2" (whnf (Set.delete "cheese") dict)
+        ]
   , env (Set.fromList <$> dictAmEn) $ \dictA ->
     env (Set.fromList <$> dictBrEn) $ \dictB ->
     env (Set.fromList <$> randomStrs) $ \dictSmall ->
       bgroup "combine"
-        [ bench "union" (nf (uncurry Set.union) (dictA, dictB))
-        , bench "intersection" (nf (uncurry Set.intersection) (dictA, dictB))
-        , bench "difference" (nf (uncurry Set.difference) (dictA, dictB))
-        , bench "append" (nf (uncurry setAppend) (dictSmall, dictSmall))
-        , bench "prefixes" (nf setPrefixes dictA)
-        , bench "suffixes" (nf setSuffixes dictB) ]
+        [ bench "union" (whnf (uncurry Set.union) (dictA, dictB))
+        , bench "intersection" (whnf (uncurry Set.intersection) (dictA, dictB))
+        , bench "difference" (whnf (uncurry Set.difference) (dictA, dictB))
+        , bench "append" (whnf (uncurry setAppend) (dictSmall, dictSmall))
+        , bench "prefixes" (whnf setPrefixes dictA)
+        , bench "suffixes" (whnf setSuffixes dictB) ]
   ]
 
 setAppend :: (Ord c) => Set [c] -> Set [c] -> Set [c]
@@ -126,9 +136,9 @@ benchTMap :: Benchmark
 benchTMap = bgroup "TMap" 
   [ bgroup "construction"
       [ env dictAmEnShuffled $ \dict ->
-          bench "fromList" $ nf TMap.fromList [(w, length w) | w <- dict]
+          bench "fromList" $ whnf TMap.fromList [(w, length w) | w <- dict]
       , env (sort <$> dictAmEn) $ \sortedDict ->
-          bench "fromAscList" $ nf TMap.fromAscList [(w, length w) | w <- sortedDict]
+          bench "fromAscList" $ whnf TMap.fromAscList [(w, length w) | w <- sortedDict]
       ]
   , env (lenTMap <$> dictAmEn) $ \dict ->
       bgroup "query"
@@ -138,6 +148,15 @@ benchTMap = bgroup "TMap"
         , env randomStrs $ \qs ->
             bench "match" (nf (\dict' -> map (`TMap.member` dict') qs) dict) ]
   , env (lenTMap <$> dictAmEn) $ \dict ->
+      bgroup "single-item"
+        [ bench "insert1" (whnf (TMap.insert "wwwwwwwwwwwwwwww" 1) dict)
+        , bench "insert2" (whnf (TMap.insert "cheese" 1) dict)
+        , bench "delete1" (whnf (TMap.delete "wwwwwwwwwwwwwwww") dict)
+        , bench "delete2" (whnf (TMap.delete "cheese") dict)
+        , bench "alter1" (whnf (TMap.alter alterFn "wwwwwwwwwwwwwwww") dict)
+        , bench "alter2" (whnf (TMap.alter alterFn "cheese") dict)
+        ]
+  , env (lenTMap <$> dictAmEn) $ \dict ->
       bgroup "traversal"
         [ bench "fmap" (nf (fmap (+3)) dict)
         , bench "foldMap" (nf (foldMap Sum) dict) ]
@@ -145,11 +164,15 @@ benchTMap = bgroup "TMap"
     env (lenTMap <$> dictBrEn) $ \dictB ->
     env (lenTMap <$> randomStrs) $ \dictSmall ->
       bgroup "combine"
-        [ bench "union" (nf (uncurry TMap.union) (dictA, dictB))
-        , bench "intersection" (nf (uncurry TMap.intersection) (dictA, dictB))
-        , bench "difference" (nf (uncurry TMap.difference) (dictA, dictB))
-        , bench "append" (nf (uncurry tmapProd) (dictSmall, dictSmall)) ]
+        [ bench "union" (whnf (uncurry TMap.union) (dictA, dictB))
+        , bench "intersection" (whnf (uncurry TMap.intersection) (dictA, dictB))
+        , bench "difference" (whnf (uncurry TMap.difference) (dictA, dictB))
+        , bench "append" (whnf (uncurry tmapProd) (dictSmall, dictSmall)) ]
   ]
+
+alterFn :: Maybe Int -> Maybe Int
+alterFn Nothing = Just 1000
+alterFn (Just a) = if even a then Just a else Nothing
 
 lenTMap :: (Ord c) => [[c]] -> TMap c Int
 lenTMap dict = TMap.fromList [(w, length w) | w <- dict]
@@ -161,9 +184,9 @@ benchMap :: Benchmark
 benchMap = bgroup "Map" 
   [ bgroup "construction"
       [ env dictAmEnShuffled $ \dict ->
-          bench "fromList" $ nf Map.fromList [(w, length w) | w <- dict]
+          bench "fromList" $ whnf Map.fromList [(w, length w) | w <- dict]
       , env (sort <$> dictAmEn) $ \sortedDict ->
-          bench "fromAscList" $ nf Map.fromAscList [(w, length w) | w <- sortedDict]
+          bench "fromAscList" $ whnf Map.fromAscList [(w, length w) | w <- sortedDict]
       ]
   , env (lenMap <$> dictAmEn) $ \dict ->
       bgroup "query"
@@ -173,6 +196,15 @@ benchMap = bgroup "Map"
         , env randomStrs $ \qs ->
             bench "match" (nf (\dict' -> map (`Map.member` dict') qs) dict) ]
   , env (lenMap <$> dictAmEn) $ \dict ->
+      bgroup "single-item"
+        [ bench "insert1" (whnf (Map.insert "wwwwwwwwwwwwwwww" 1) dict)
+        , bench "insert2" (whnf (Map.insert "cheese" 1) dict)
+        , bench "delete1" (whnf (Map.delete "wwwwwwwwwwwwwwww") dict)
+        , bench "delete2" (whnf (Map.delete "cheese") dict)
+        , bench "alter1" (whnf (Map.alter alterFn "wwwwwwwwwwwwwwww") dict)
+        , bench "alter2" (whnf (Map.alter alterFn "cheese") dict)
+        ]
+  , env (lenMap <$> dictAmEn) $ \dict ->
       bgroup "traversal"
         [ bench "fmap" (nf (fmap (+3)) dict)
         , bench "foldMap" (nf (foldMap Sum) dict) ]
@@ -180,10 +212,10 @@ benchMap = bgroup "Map"
     env (lenMap <$> dictBrEn) $ \dictB ->
     env (lenMap <$> randomStrs) $ \dictSmall ->
       bgroup "combine"
-        [ bench "union" (nf (uncurry Map.union) (dictA, dictB))
-        , bench "intersection" (nf (uncurry Map.intersection) (dictA, dictB))
-        , bench "difference" (nf (uncurry Map.difference) (dictA, dictB))
-        , bench "append" (nf (uncurry mapProd) (dictSmall, dictSmall)) ]
+        [ bench "union" (whnf (uncurry Map.union) (dictA, dictB))
+        , bench "intersection" (whnf (uncurry Map.intersection) (dictA, dictB))
+        , bench "difference" (whnf (uncurry Map.difference) (dictA, dictB))
+        , bench "append" (whnf (uncurry mapProd) (dictSmall, dictSmall)) ]
   ]
 
 lenMap :: (Ord c) => [[c]] -> Map [c] Int
